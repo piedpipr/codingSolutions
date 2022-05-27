07c7cab19d04804d83003c1726d73becafc5ea61
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-05-28 00:14:03 +0600

    Initial solution Main class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] arg){
+        //Main Class&Method for Solution
+        //Change the Main to filename for individual solutions
+        int n = 56;
+
+        }
+    }
+}
